650b28ca8e9aee966bb2a01f38a652833cca5a6a
rameez jawaid <rameez.jawaid@yayvo.com>
2020-04-18 22:39:05 +0500

    feedback implementation - ongoing

--- arabic/head.php
+++ arabic/head.php
@@ -14,6 +14,7 @@
   <link rel="stylesheet" href="css/kursor.css">
   <link rel="stylesheet" href="css/main.css">
   <link rel="stylesheet" href="css/glb.css">
+  <link rel="stylesheet" href="css/feedback.css">
   <link rel="stylesheet" href="css/responsive.css">
   <link href="https://vjs.zencdn.net/7.7.5/video-js.css" rel="stylesheet" />
 
